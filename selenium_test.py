c5cb380ea3575389886d15b56c9a76769a48e555
Alex Liu <aleliu@suncor.com>
2023-04-17 14:02:14 -0600

    test

--- selenium_test.py
+++ selenium_test.py
@@ -8,14 +8,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 class LoginForm(unittest.TestCase):
+
     def setUp(self):
 
         # Put your username and authkey below
         # You can find your authkey at crossbrowsertesting.com/account
         self.username = os.environ.get('CBT_USERNAME')
         self.authkey = os.environ.get('CBT_AUTHKEY')
-        # self.username = 'liuzhendell@gmail.com'
-        # self.authkey = 'ZmmSFtUqSEBvGm5mughzcvREbAau1qNz'
 
         self.api_session = requests.Session()
         self.api_session.auth = (self.username, self.authkey)
@@ -30,61 +29,59 @@ class LoginForm(unittest.TestCase):
         caps['screenResolution'] = '1366x768'
         caps['username'] = self.username
         caps['password'] = self.authkey
-        print(self.username)
+        caps['record_video'] = 'true'
 
-        print(self.authkey)
         self.driver = webdriver.Remote(
             desired_capabilities=caps,
-            # command_executor="http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub" % (self.username, self.authkey)
+            # command_executor="http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub"%(self.username,self.authkey)
             command_executor="http://hub.crossbrowsertesting.com:80/wd/hub"
         )
 
         self.driver.implicitly_wait(20)
 
-    def test_CBT(self):
 
-        try:
-            self.driver.get(
-                'http://crossbrowsertesting.github.io/login-form.html')
-            self.driver.maximize_window()
-            self.driver.find_element_by_name('username').send_keys(
-                'tester@crossbrowsertesting.com')
-            self.driver.find_element_by_name('password').send_keys('test123')
-            self.driver.find_element_by_css_selector(
-                'body > div > div > div > div > form > div.form-actions > button').click()
+def test_CBT(self):
 
-            elem = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located(
-                    (By.XPATH, '//*[@id=\"logged-in-message\"]/h2'))
-            )
+    try:
+        self.driver.get('http://crossbrowsertesting.github.io/login-form.html')
+        self.driver.maximize_window()
+        self.driver.find_element_by_name('username').send_keys(
+            'tester@crossbrowsertesting.com')
+        self.driver.find_element_by_name('password').send_keys('test123')
+        self.driver.find_element_by_css_selector(
+            'body &gt; div &gt; div &gt; div &gt; div &gt; form &gt; div.form-actions &gt; button').click()
 
-            welcomeText = elem.text
-            self.assertEqual(
-                "Welcome tester@crossbrowsertesting.com", welcomeText)
+        elem = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id=\"logged-in-message\"]/h2'))
+        )
 
-            print("Taking snapshot")
-            print("Taking snapshot")
-            snapshot_hash = self.api_session.post(
-                'https://crossbrowsertesting.com/api/v3/selenium/' + self.driver.session_id + '/snapshots').json()['hash']
+        welcomeText = elem.text
+        self.assertEqual("Welcome tester@crossbrowsertesting.com", welcomeText)
 
-            self.test_result = 'pass'
+        print("Taking snapshot")
+        snapshot_hash = self.api_session.post(
+            'https://crossbrowsertesting.com/api/v3/selenium/' + self.driver.session_id + '/snapshots').json()['hash']
 
-        except AssertionError as e:
-            # log the error message, and set the score to "during tearDown()".
-            self.api_session.put('https://crossbrowsertesting.com/api/v3/selenium/' + self.driver.session_id + '/snapshots/' + snapshot_hash,
-                                 data={'description': "AssertionError: " + str(e)})
-            self.test_result = 'fail'
-            raise
+        self.test_result = 'pass'
 
-    def tearDown(self):
-        print("Done with session %s" % self.driver.session_id)
-        self.driver.quit()
-        # Here we make the api call to set the test's score.
-        # Pass it it passes, fail if an assertion fails, unset if the test didn't finish
-        if self.test_result is not None:
-            self.api_session.put('https://crossbrowsertesting.com/api/v3/selenium/' + self.driver.session_id,
-                                 data={'action': 'set_score', 'score': self.test_result})
+    except AssertionError as e:
+        # log the error message, and set the score to "during tearDown()".
+        self.api_session.put('https://crossbrowsertesting.com/api/v3/selenium/' + self.driver.session_id + '/snapshots/' + snapshot_hash,
+                             data={'description': "AssertionError: " + str(e)})
+        self.test_result = 'fail'
+        raise
 
 
-if __name__ == '__main__':
+def tearDown(self):
+    print("Done with session %s" % self.driver.session_id)
+    self.driver.quit()
+    # Here we make the api call to set the test's score.
+    # Pass it it passes, fail if an assertion fails, unset if the test didn't finish
+    if self.test_result is not None:
+        self.api_session.put('https://crossbrowsertesting.com/api/v3/selenium/' + self.driver.session_id,
+                             data={'action': 'set_score', 'score': self.test_result})
+
+
+if name == 'main':
     unittest.main()
